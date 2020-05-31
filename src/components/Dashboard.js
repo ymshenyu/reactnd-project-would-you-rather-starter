52eee9fc587afab3a24f48ccbdf854aaa2db4c98
ymshenyu <ymshenyu@gmail.com>
2020-05-31 17:53:01 +0800

    wip: dash board page - 2

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,13 +3,12 @@ import { connect } from 'react-redux'
 import { makeStyles } from '@material-ui/core/styles'
 import Paper from '@material-ui/core/Paper'
 import List from '@material-ui/core/List'
-import Divider from '@material-ui/core/Divider'
 import ListItem from '@material-ui/core/ListItem'
-import ListItemIcon from '@material-ui/core/ListItemIcon'
-import ListItemText from '@material-ui/core/ListItemText'
 import ButtonGroup from '@material-ui/core/ButtonGroup'
 import Button from '@material-ui/core/Button'
 import Avatar from '@material-ui/core/Avatar'
+import Grid from '@material-ui/core/Grid'
+import Typography from '@material-ui/core/Typography'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -25,6 +24,13 @@ const useStyles = makeStyles((theme) => ({
         }
     },
     question: {
+        width: '100%'
+    },
+    avatar: {
+        height: '128px',
+        width: '128px'
+    },
+    paper: {
         width: '100%'
     }
 }))
@@ -54,10 +60,42 @@ const DashBoard = (props) => {
                 <List>
                     {matchQuestion.map((question) => (
                         <ListItem key={questions[question].id}>
-                            <Paper elevation={3} className={classes.question}>
+                            {/* <Paper elevation={3} className={classes.question}>
                                 <Avatar src={users[questions[question].author].avatarURL}
                                     alt={`Avatar of ${questions[question].author}`} variant='square' />
                                 <span>{questions[question].id}</span>
+                            </Paper> */}
+                            <Paper className={classes.paper}>
+                                <Grid container spacing={2}>
+                                    <Grid item>
+                                        <Avatar src={users[questions[question].author].avatarURL}
+                                            alt={`Avatar of ${questions[question].author}`} variant='square'
+                                            className={classes.avatar}
+                                        />
+                                    </Grid>
+                                    <Grid item xs={12} sm container>
+                                        <Grid item xs container direction="column" spacing={2}>
+                                            <Grid item xs>
+                                                <Typography gutterBottom variant="subtitle1">
+                                                    {users[questions[question].author].name} asks:
+                                                </Typography>
+                                                <Typography variant="body2" gutterBottom>
+                                                    Would you rather
+                                                </Typography>
+                                                <Typography variant="body2" color="textSecondary">
+                                                    ...{questions[question].optionOne.text}...
+                                                </Typography>
+                                            </Grid>
+                                            <Grid item>
+                                                <Typography variant="body2" style={{ marginBottom: '10px' }}>
+                                                    <Button variant="contained" color="primary">
+                                                        View
+                                                    </Button>
+                                                </Typography>
+                                            </Grid>
+                                        </Grid>
+                                    </Grid>
+                                </Grid>
                             </Paper>
                         </ListItem>
                     ))}
@@ -67,10 +105,10 @@ const DashBoard = (props) => {
     )
 }
 
-const mapStateToProps = ({ users, questions, authedUser }) => ({
-    users,
-    questions,
-    authedUser
-})
+const mapStateToProps = ({ users, questions, authedUser}) => ({
+                users,
+                questions,
+                authedUser
+            })
 
 export default connect(mapStateToProps)(DashBoard)
