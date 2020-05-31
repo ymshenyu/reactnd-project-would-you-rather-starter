7f592badffe9ee15d8fae24df3e5e3099a7ca9f6
ymshenyu <ymshenyu@gmail.com>
2020-05-31 10:06:39 +0800

    finalize auth page

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -6,7 +6,7 @@ import logo from '../logo/react.svg'
 import MenuItem from '@material-ui/core/MenuItem'
 import Select from '@material-ui/core/Select'
 import Button from '@material-ui/core/Button'
-import FormControl from '@material-ui/core/FormControl'
+import { setAuthedUser } from '../actions/authedUser'
 
 
 const useStyles = makeStyles((theme) => ({
@@ -15,9 +15,11 @@ const useStyles = makeStyles((theme) => ({
         flexWrap: 'wrap',
         justifyContent: 'center',
         '& > *': {
+            display: 'flex',
+            flexDirection: 'column',
             margin: theme.spacing(1),
             width: '40vw',
-            height: '60vh',
+            height: '80vh'
         }
     }
 }))
@@ -27,15 +29,20 @@ const Auth = (props) => {
     const { users } = props
     const classes = useStyles()
     const [value, setValue] = useState('')
+    const handleOnClick = (e) => {
+        e.preventDefault()
+        const { dispatch } = props
+        dispatch(setAuthedUser(value))
+    }
     return (
         <div className={classes.root}>
             <Paper elevation={3}>
                 <Paper elevation={3} style={{ backgroundColor: '#f2f2f2', textAlign: 'center' }}>
-                    <h2 style={{ marginTop: '0px' }}>Welcome to the Would You Rather App</h2>
-                    <h3 style={{ marginTop: '0px' }}>Please sign in to continue</h3>
+                    <h2>Welcome to the Would You Rather App</h2>
+                    <h3>Please sign in to continue</h3>
                 </Paper>
-                <img src={logo} alt='logo' style={{ height: '60%', display: 'block', margin: '0 auto' }} />
-                <div>
+                <img src={logo} alt='logo' style={{ maxHeight: '60%', maxWidth: '100%', margin: '0 auto', marginTop: '20px' }} />
+                <div style={{ marginTop: 'auto' }}>
                     <Select value={value} onChange={(e) => setValue(e.target.value)} style={{ width: '100%' }}>
                         {Object.keys(users).map(user => (
                             <MenuItem key={users[user].id} value={users[user].id}>
@@ -43,7 +50,10 @@ const Auth = (props) => {
                             </MenuItem>
                         ))}
                     </Select>
-                    <Button variant='contained' color="primary" style={{ marginTop: '40px', width: '100%' }}>Login</Button>
+                    <Button variant='contained' color="primary" style={{ marginTop: '40px', width: '100%' }}
+                        disabled={value === ''} onClick={handleOnClick}>
+                        Login
+                    </Button>
                 </div>
             </Paper>
         </div>
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,6 +5,8 @@ import Toolbar from '@material-ui/core/Toolbar'
 import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button'
 import { connect } from 'react-redux'
+import { setAuthedUser } from '../actions/authedUser'
+import Avatar from '@material-ui/core/Avatar'
 
 const useStyles = makeStyles((theme) => ({
     root: {
@@ -16,11 +18,24 @@ const useStyles = makeStyles((theme) => ({
     title: {
         flexGrow: 1,
     },
+    space: {
+        marginRight: theme.spacing(2)
+    },
+    center: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center'
+    }
 }));
 
 function ButtonAppBar(props) {
     const classes = useStyles();
-    const { authedUser } = props
+    const { authedUser, users } = props
+    const handleLogout = (e) => {
+        e.preventDefault()
+        const { dispatch } = props
+        dispatch(setAuthedUser(null))
+    }
     return (
         <div className={classes.root}>
             <AppBar position="static">
@@ -30,7 +45,16 @@ function ButtonAppBar(props) {
                     <Button variant='contained' color="secondary" className={classes.menuButton}>Leader Board</Button>
                     <Typography className={classes.title}></Typography>
                     {authedUser !== null
-                        ? <Button variant='contained' color="secondary">Logout</Button>
+                        ?
+                        <div className={classes.center}>
+                            <Avatar className={classes.space} alt={users[authedUser].name} src={users[authedUser].avatarURL} />
+                            <Typography variant='h6' className={classes.space}>
+                                {users[authedUser].name}
+                            </Typography>
+                        </div>
+                        : null}
+                    {authedUser !== null
+                        ? <Button variant='contained' color="secondary" onClick={handleLogout}>Logout</Button>
                         : null}
                 </Toolbar>
             </AppBar>
@@ -38,8 +62,9 @@ function ButtonAppBar(props) {
     );
 }
 
-const mapStateToProps = ({ authedUser }) => {
+const mapStateToProps = ({ users, authedUser }) => {
     return ({
+        users,
         authedUser
     })
 }
